118863480670be9a2ee9c956243268c772319e21
Olive Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2024-01-09 15:32:32 -0500

    installed nannou

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use nannou::prelude::*;
+
+struct Model {
+    _window: window::Id,
+}
+
 fn main() {
-    println!("Hello, world!");
+    nannou::app(model)
+        .update(update)
+        .run();
+}
+
+fn model(app: &App) -> Model {
+    let win = app.new_window()
+        .size(800, 600)
+        .view(view)
+        .build()
+        .unwrap();
+
+    Model {
+        _window: win,
+    }
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {
+}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let win = app.window_rect();
+
+    let draw = app.draw();
+
+    draw.background().color(LIGHTGREY);
+    draw.line()
+        .start(pt2(-win.w() / 2.0, -win.h() / 3.0))
+        .end(pt2(win.w() / 2.0, -win.h() / 3.0))
+        .color(BLACK)
+        .stroke_weight(5.0);
+
+    draw.to_frame(app, &frame).unwrap();
 }
