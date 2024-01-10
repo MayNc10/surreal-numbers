45848fb9be4df2a8185793356641a5576f16c52e
May <MayflowerC10@gmail.com>
2024-01-10 16:54:43 -0500

    WIP: refactor evaluation types

--- src/computer.rs
+++ src/computer.rs
@@ -1,13 +1,41 @@
-use crate::hackenbush::{Game, Graph};
+use crate::hackenbush::{Color, Game, Graph};
 
-pub fn find_best_move(_game: &Game) -> usize {
+pub struct Position {
+    pub score: i32,
+    pub best_move: Option<usize>,
+}
+
+pub fn find_best_move(_game: &Game, _player: Color) -> Position {
     panic!("TODO")
 }
 
 // + -> blue
 // - -> red
 
-fn find_best_move_subgraph(graph: &Graph) -> usize {
-    // Start with the simplest 
+const ONE_BLUE: i32 = 1;
+const ONE_RED: i32 = -1;
 
+fn find_best_move_subgraph(graph: &Graph, player: Color) -> Position {
+    // Brute force move search
+    // For every move, make that move, then compute the value of the position
+    // a position where red loses / blue wins = +1, red wins / blue loses = -1
+
+    // This is bad, but tells us if there's only one edge in the graph (i.e., one move)
+
+    if let Some(g_move) = graph.edge_weights().next() && graph.edge_weights().skip(1).next().is_none() {
+        return Position {
+            score: match g_move { Color::Blue => ONE_BLUE, Color::Red => ONE_RED },
+            best_move: if g_move == player {
+                Some( graph.edge_indices().next().unwrap().index() )
+            } else { None }
+        }
+    }
+
+    for edge in graph.edge_weights() {
+        if edge == player {
+
+        }
+    }
+
+    None
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(let_chains)]
+
 mod hackenbush;
 mod computer;
 mod app;
